a3a40c8dfaf5955b0fa07839c6caff16d7e3de17
Hery1235 <mh47098@gmail.com>
2025-03-29 15:42:40 +0000

    images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import profile_image from "../../../assets/hero/waqar.jpeg";
 
 export const Hero = () => {
   return (
@@ -9,14 +10,17 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, Waqar Ali Khan</h1>
         <p className={styles.description}>
-        I'm an electrical engineer with 16 years of experience in design and installation. Passionate about BIM, digital twin technology, and AI-driven solutions, I aim to revolutionize building modeling for smarter, more efficient designs
+          I'm an electrical engineer with 16 years of experience in design and
+          installation. Passionate about BIM, digital twin technology, and
+          AI-driven solutions, I aim to revolutionize building modeling for
+          smarter, more efficient designs
         </p>
         <a href="mailto:mh47098@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/waqar.jpeg")}
+        src={profile_image}
         alt="Hero image of me"
         className={styles.heroImg}
       />
